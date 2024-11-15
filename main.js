ea62292224a39f351432b3909195327be4732c3d
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-11-15 20:08:03 +0600

    worked on home section

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "Data Scientiest"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
